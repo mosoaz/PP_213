7b05e9dbced8b6815702d01aa24376d6f4f1798c
Andrey N <mosoaz@mail.ru>
2023-07-02 15:33:54 +0300

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String message;
+
+    public String getCat() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
